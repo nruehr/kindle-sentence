db054603232d4b452395c102994a2240b8cfb54a
Nick <fender_strat128@yahoo.com>
2022-09-03 19:41:06 +0200

    add main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+import os
+import re
+import win_unicode_console
+import clipboard
+
+# export the sentences from a kindle my clippings text file
+# open the textfile, read it and save it as text 
+# might have to adjust the drive letter
+with open('H:/documents/My Clippings.txt', encoding='utf-8') as notes:
+    text = notes.read()
+
+# rearrange the text
+text2 = re.sub("- Your Highlight on page .*\n\n", "", text)
+text3 = re.sub("==========", "", text2)
+
+# copy the text to the clipboard
+clipboard.copy(text3)
